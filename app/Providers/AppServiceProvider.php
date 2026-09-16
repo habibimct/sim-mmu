682cdbfc13e033598dbb5f476c8f40436d6b61b1
Habibi <irhabibi6@gmail.com>
2026-09-17 05:55:45 +0700

    Remove forced HTTPS on local environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,10 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(config('app.env') === 'local'){
-            URL::forceScheme('https');
-        }
-
         Gate::before(function (User $user, string $ability) {
             return $user->hasPermission($ability) ? true : null;
         });
